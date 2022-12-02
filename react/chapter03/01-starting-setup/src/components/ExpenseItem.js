4615f4af8a6f4dcf4ff85da62be1d751b2086ab2
bogus <qazxwd44@hanmail.net>
2022-12-02 23:37:56 +0900

    기본 css추가

--- react/chapter03/01-starting-setup/src/components/ExpenseItem.js
+++ react/chapter03/01-starting-setup/src/components/ExpenseItem.js
@@ -1,3 +1,13 @@
+import "./ExpenseItem.css";
+
 export default function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2002</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.87</div>
+      </div>
+    </div>
+  );
 }
