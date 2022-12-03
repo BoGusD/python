c3165f50485c2b3336c8e12ef78f74c8649e7cc1
bogus <qazxwd44@hanmail.net>
2022-12-03 15:46:28 +0900

    컴포넌트 여러개 분할

--- react/chapter03/01-starting-setup/src/App.js
+++ react/chapter03/01-starting-setup/src/App.js
@@ -6,28 +6,24 @@ export default function App() {
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      buyer: "Mike",
     },
     {
       id: "e2",
       title: "New TV",
       amount: 799.49,
       date: new Date(2021, 2, 12),
-      buyer: "Tom",
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      buyer: "Jane",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      buyer: "Nina",
     },
   ];
 
