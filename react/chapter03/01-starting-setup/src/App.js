59f86409d80bafae3112a6eed90114d4b06d4138
bogus <qazxwd44@hanmail.net>
2022-12-03 15:45:13 +0900

    컴포넌트 여러개 분할

--- react/chapter03/01-starting-setup/src/App.js
+++ react/chapter03/01-starting-setup/src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
-
+import Expense from "./components/Expense";
 export default function App() {
   const expenses = [
     {
@@ -7,45 +6,35 @@ export default function App() {
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
+      buyer: "Mike",
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      buyer: "Tom",
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
+      buyer: "Jane",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
+      buyer: "Nina",
     },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expense items={expenses} />
     </div>
   );
 }
---
+++ react/chapter03/01-starting-setup/src/components/Expense.js
@@ -0,0 +1,35 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expense.css";
+
+export default function items(props) {
+  return (
+    <>
+      <div className="expenses">
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+          buyer={props.items[0].buyer}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+          buyer={props.items[1].buyer}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+          buyer={props.items[2].buyer}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+          buyer={props.items[3].buyer}
+        ></ExpenseItem>
+      </div>
+    </>
+  );
+}
