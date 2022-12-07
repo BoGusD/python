83cb76a26272fb6bc696625e40999e4100bd73d4
bogus <qazxwd44@hanmail.net>
2022-12-07 22:12:57 +0900

    렌더링 리스트 및 조건부 설정 완료

--- react/chapter03~05/src/components/Expenses/Expense.js
+++ react/chapter03~05/src/components/Expenses/Expense.js
@@ -3,6 +3,8 @@ import Card from "../UI/Card";
 import "./Expense.css";
 import ExpensesFilter from "./ExpenseFilter";
 import { useState } from "react";
+import ExpenseList from "./ExpenseList";
+import ExpenseChart from "./ExpenseChart";
 
 export default function Expense(props) {
   const [filterYear, setFilterYear] = useState("2020");
@@ -15,18 +17,6 @@ export default function Expense(props) {
     return expense.date.getFullYear().toString() === filterYear;
   });
 
-  let expenseContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
   return (
     <div>
       <Card className="expenses">
@@ -34,7 +24,8 @@ export default function Expense(props) {
           selected={filterYear}
           onSaveFilterData={filterChangeHandler}
         />
-        {expenseContent}
+        <ExpenseChart expenses={filteredExpenses} />
+        <ExpenseList items={filteredExpenses} />
       </Card>
     </div>
   );
