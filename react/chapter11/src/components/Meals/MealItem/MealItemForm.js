5682e79fe35542d43f535d51f6e72f478c6d38d6
bogus <qazxwd44@hanmail.net>
2022-12-15 18:32:29 +0900

    장바구니 로직 추가

--- react/chapter11/src/components/Meals/MealItem/MealItemForm.js
+++ react/chapter11/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,31 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 export default function MealItemForm(props) {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
+        ref={amountInputRef}
         input={{
           id: "amount_" + props.id, // this changed!
           type: "number",
@@ -15,7 +35,8 @@ export default function MealItemForm(props) {
           defaultValue: "1",
         }}
       />
-      <button>+Add</button>
+      <button>추가</button>
+      {!amountIsValid && <p>다음 범위 사이의 유효값을 입력해주세요 (1-5).</p>}
     </form>
   );
 }
