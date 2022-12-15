050883b54eca63455e5de3116df3a3c7287afadf
bogus <qazxwd44@hanmail.net>
2022-12-15 17:30:00 +0900

    store 생성

--- react/chapter11/src/components/Layout/HeaderCartButton.js
+++ react/chapter11/src/components/Layout/HeaderCartButton.js
@@ -1,7 +1,10 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context.js";
 import CartIcon from "../Cart/CartIcon.js";
 import classes from "./HeaderCartButton.module.css";
 
 export default function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
