83c2cb8eae248931dbc48c609dfa7dde3652f966
bogus <qazxwd44@hanmail.net>
2022-12-15 10:41:01 +0900

    장바구니 버튼 추가

--- react/chapter11/src/components/Layout/Header.js
+++ react/chapter11/src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import classes from "./Header.module.css";
 import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 export default function Header(props) {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="음식 뷔페 이미지" />
