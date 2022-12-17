165c3c6d705faea9eb594d106a7e29e96ae3d671
bogus <qazxwd44@hanmail.net>
2022-12-17 18:04:34 +0900

    버튼 생성을 통한 랜더링 과정 구분

--- react/chapter12/src/App.js
+++ react/chapter12/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showPargraph, setShowPargraph] = useState(false);
+  console.log("App Running");
+
+  const toggleParagraphHandler = () => {
+    setShowPargraph((prevShowParagraph) => !prevShowParagraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showPargraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Pargraph!</Button>
     </div>
   );
 }
