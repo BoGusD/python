9e62869a1899511049bd8d502a3f246776ab6ce3
bogus <qazxwd44@hanmail.net>
2022-12-19 17:16:21 +0900

    usecallback을 통한 closure 작동 원리 구현

--- react/chapter12/src/App.js
+++ react/chapter12/src/App.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 import "./App.css";
 
 function App() {
   const [showPargraph, setShowPargraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
   console.log("App Running");
 
-  const toggleParagraphHandler = () => {
-    setShowPargraph((prevShowParagraph) => !prevShowParagraph);
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowPargraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showPargraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling!</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Pargraph!</Button>
     </div>
   );
