0dc2e3738b813e9826c247de34fca0c41f2ddff7
bogus <qazxwd44@hanmail.net>
2022-12-19 16:48:22 +0900

    usememo의 기능

--- react/chapter12/src/components/Demo/DemoOutput.js
+++ react/chapter12/src/components/Demo/DemoOutput.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-export default function DemoOutput(props) {
+const DemoOutput = (props) => {
   console.log("DemoOutPut Running");
   return <p>{props.show ? "This is New!" : ""}</p>;
-}
+};
+
+export default React.memo(DemoOutput);
