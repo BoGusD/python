13bf61e8fa2db935e693ef914b2bf9f2808b81c6
bogus <qazxwd44@hanmail.net>
2022-12-21 00:40:23 +0900

    custom hook multi 사용

--- react/chapter15/src/hooks/use-counter.js
+++ react/chapter15/src/hooks/use-counter.js
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react";
 
 // 커스텀 훅 파일은 항상 use로 시작하는 상수로 설정해야된다.(재사용하려는 로직을 추가하면 됨)
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
