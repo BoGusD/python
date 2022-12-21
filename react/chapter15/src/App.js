738568d860399d2fd2387dcec0f84f6611db2332
bogus <qazxwd44@hanmail.net>
2022-12-21 20:54:17 +0900

    custom hook 로직 구현 완료

--- react/chapter15/src/App.js
+++ react/chapter15/src/App.js
@@ -28,7 +28,7 @@ function App() {
       },
       transformTask
     );
-  }, []);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
