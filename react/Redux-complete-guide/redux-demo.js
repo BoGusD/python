ed0ab0fffbcf0526081bf5276fa93f17b8453688
bogus <qazxwd44@hanmail.net>
2023-01-11 14:25:46 +0900

    typescript 시작

--- react/Redux-complete-guide/redux-demo.js
+++ react/Redux-complete-guide/redux-demo.js
@@ -2,13 +2,20 @@ const redux = require("redux");
 
 // 리듀서 함수
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
 };
 // 어떤 리듀서가 그 저장소를 변경하는지 저장소가 알아야 함
 const store = redux.createStore(counterReducer);
-console.log(store.getState());
+
 //구독
 const counterSubscriber = () => {
   //getState()는 createStore()로 생성된 저장소에서 사용할 수 있는 메소드
@@ -23,3 +30,4 @@ store.subscribe(counterSubscriber);
 //action
 //type은 자바스크립트 내장 객체
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
