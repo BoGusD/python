effbff764e03c93f0380ce34c2f414be5d1c2d9d
bogus <qazxwd44@hanmail.net>
2023-06-03 10:50:36 +0900

    type 가드

--- typescript/chapter06/src/app.ts
+++ typescript/chapter06/src/app.ts
@@ -22,3 +22,59 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  //객체에는 속성이 있는지 여부는 알려주지 않는다. ( 타입가드의 필요성)
+  //   if (typeof emp == "object") {
+
+  //if 문 타입가드
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("startDate: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving ...");
+  }
+}
+class Truck {
+  drive() {
+    console.log("Driving a Truk...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading cargo ...." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+//클래스 사용 가능 이유 자바스크립트가 클래스와 생성자 함수를 지원하기 때문
+//인터페이스는 사용 불가
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
