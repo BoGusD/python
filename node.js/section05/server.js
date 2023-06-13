a3d3340d82a3aa78206b60a22e2713484f3c5228
bogus <qazxwd44@hanmail.net>
2023-06-13 18:47:26 +0900

    use와 post get의 옵션차이

--- node.js/section05/server.js
+++ node.js/section05/server.js
@@ -1,17 +1,26 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const server = express();
 
 //middleWare
-server.use((req, res, next) => {
-  console.log("In the middleware!");
-  next(); // Allows the request to continue to the next middleware in line
+
+//urlencoded 본문만 구문 분석하고 Content-Type 헤더가 type 옵션과 일치하는 요청만 보는 미들웨어를 반환합니다.
+//extended: false << 비표준 대상의 분석이 가능한지를 나타낸다.
+//use는 모든 http에 반응한다.
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type+"text" name="title"><button type="submit">Add Product</button></input></form>'
+  );
+});
+server.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-server.use((req, res, next) => {
-  console.log("In another middleware!");
+server.use("/", (req, res, next) => {
   res.send("<h1>Hello from Express!</h1>");
 });
 
