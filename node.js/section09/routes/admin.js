e9a6cb00abef05ceadafa1e71c0c431f1dde6a6b
unknown <qazxwd44@hanmail.net>
2023-06-23 14:58:09 +0900

    동적 라우트 및 고급 모델

--- node.js/section09/routes/admin.js
+++ node.js/section09/routes/admin.js
@@ -1,18 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const adminController = require('../controllers/admin');
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
+//항상 코드는 위에서 아래로 파싱된다.
+
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get("/products", adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
+
+//router.get("/products/delete") >> productId 위에 사용해줘야함
+// 아래에 위치해 있으면 동적 세그먼트 취급을 받기 때문에 productId 로직에 먹힘
+
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product", adminController.postDeleteProduct);
 
 module.exports = router;
--- node.js/section09/routes/shop.js
+++ node.js/section09/routes/shop.js
@@ -1,25 +1,25 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const shopController = require("../controllers/shop");
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-//항상 코드는 위에서 아래로 파싱된다.
+router.get('/', shopController.getIndex);
 
-router.get("/", shopController.getIndex);
+router.get('/products', shopController.getProducts);
 
-router.get("/products", shopController.getProducts);
+router.get('/products/:productId', shopController.getProduct);
 
-//router.get("/products/delete") >> 동적 경로가 아니기에 productId 위에 사용해줘야함
+router.get('/cart', shopController.getCart);
 
-router.get("/products/:productId", shopController.getProduct);
+router.post('/cart', shopController.postCart);
 
-router.get("/cart", shopController.getCart);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get("/orders", shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
-router.get("/checkout", shopController.getCheckout);
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
